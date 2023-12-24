6265a3d125105f39a2637f5c28b4000264053fd9
rahuldate8 <rahul.date8@gmail.com>
2023-12-24 15:09:48 +0000

    Create new DEV VPC 1

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "blog" {
   # vpc_security_group_ids = [aws_security_group.blog.id]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_ids = module.blog_vpc.public_subnets[0]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
