f05102ec01d95172e1273a5109b23a84be8e9502
rahuldate8 <rahul.date8@gmail.com>
2023-12-24 15:49:22 +0000

    Load Balancer 11

--- main.tf
+++ main.tf
@@ -115,6 +115,12 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
   }
 
