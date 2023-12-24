2bc29a2eaed7a85e3fb0ed296364bf99d33cf310
rahuldate8 <rahul.date8@gmail.com>
2023-12-24 15:47:00 +0000

    Load Balancer 9

--- main.tf
+++ main.tf
@@ -72,20 +72,20 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
-    }
-  ]
+  # target_groups = [
+  #  {
+  #    name_prefix      = "blog-"
+  #    backend_protocol = "HTTP"
+  #    backend_port     = 80
+  #    target_type      = "instance"
+  #    targets = {
+  #      my_target = {
+  #        target_id = aws_instance.blog.id
+  #        port = 80
+  #      }
+  #    }
+  #  }
+  #]
 
   listeners = [
     {
@@ -94,6 +94,30 @@ module "alb" {
       target_group_index = 0
     }
   ]
+
+
+  # check
+
+  listeners = {
+    ex-http = {
+      port            = 80
+      protocol        = "HTTP"      
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+    }
+  }
+
 
 
   tags = {
