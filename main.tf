bea8ef1a80d8deac2eec903795b200bda7197e38
rahuldate8 <rahul.date8@gmail.com>
2023-12-24 15:24:08 +0000

    Load Balancer

--- main.tf
+++ main.tf
@@ -61,6 +61,67 @@ resource "aws_instance" "blog" {
  }
 
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.bl.public_subnets
+  security_groups = module,blog_sg.security_group_id
+
+  target_groups = {
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  }
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+
+http_tcp_listeners = [
+  {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+  }
+]
+
+
+  tags = {
+    Environment = "dev"    
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
